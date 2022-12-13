5740daecb59c2f498832d9d908e7fde52c4a6d4b
Floris Porro <floris@streammyevent.com>
2022-12-13 20:02:52 +0100

    Initial scaffolding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import questions from '../data.json';
+
+	console.log(questions);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
