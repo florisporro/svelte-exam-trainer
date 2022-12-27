1bf8050e04229e7d4291fae6c2d3f99a786649f0
Floris Porro <floris@streammyevent.com>
2022-12-27 21:17:03 +0100

    Various styling and layout fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,6 @@
 	import '../app.pcss';
 </script>
 
-<header class="h-24 py-12 mb-12">
-	<h1 class="text-4xl text-center">Svelte Exam Trainer</h1>
-</header>
 <main class="container mx-auto p-5">
 	<slot />
 </main>
