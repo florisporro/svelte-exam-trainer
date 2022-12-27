f788d76b7f3b47d456122de835ba0a7328b91613
Floris Porro <floris@streammyevent.com>
2022-12-27 21:17:35 +0100

    Add passing grade selector

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,6 +38,8 @@
 		7: true
 	};
 
+	let passingGrade = 0.75;
+
 	function finishQuiz(quiz: QuizType) {
 		quizHistory.push({
 			topic: quiz.topic,
@@ -62,80 +64,102 @@
 				finishQuiz(event.detail);
 			}}
 		/>
+		<button
+			class="btn btn-outline"
+			on:click={() => {
+				quizActive = false;
+			}}
+		>
+			Return home (abort quiz)
+		</button>
 	</div>
-	<button
-		class="btn btn-outline"
-		on:click={() => {
-			quizActive = false;
-		}}
-	>
-		Return home (abort quiz)
-	</button>
 {:else}
-	<div class="numberOfQuestions mb-10">
-		<p>Number of questions in quiz:</p>
-		<input
-			bind:value={numberOfQuestionsIndex}
-			type="range"
-			min="0"
-			max="7"
-			class="range"
-			step="1"
-		/>
-		<div class="w-full flex justify-between text-xs px-2">
-			<span>5</span>
-			<span>10</span>
-			<span>13</span>
-			<span>16</span>
-			<span>20</span>
-			<span>40</span>
-			<span>60</span>
-			<span>All</span>
+	<div class="quizselector pb-10">
+		<div class="h-24 py-12 mb-12">
+			<h1 class="text-4xl text-center">Svelte Exam Trainer</h1>
 		</div>
-	</div>
-	<ul class="topics">
-		{#each [...topics] as topic}
-			{@const topicHistory = quizHistory.filter((q) => q.topic === topic)}
-			<li>
+		<div class="numberOfQuestions mb-10">
+			<p>Number of questions in quiz:</p>
+			<input
+				bind:value={numberOfQuestionsIndex}
+				type="range"
+				min="0"
+				max="7"
+				class="range"
+				step="1"
+			/>
+			<div class="w-full flex justify-between text-xs px-2">
+				<span>5</span>
+				<span>10</span>
+				<span>13</span>
+				<span>16</span>
+				<span>20</span>
+				<span>40</span>
+				<span>60</span>
+				<span>All</span>
+			</div>
+		</div>
+		<div class="passingGrade mb-10">
+			<p>Passing grade: {Math.round(passingGrade * 100)} %</p>
+			<input bind:value={passingGrade} type="range" min="0" max="1" class="range" step="0.05" />
+			<div class="w-full flex justify-between text-center text-xs">
+				<span>0%</span>
+				<span>10%</span>
+				<span>20%</span>
+				<span>30%</span>
+				<span>40%</span>
+				<span>50%</span>
+				<span>60%</span>
+				<span>70%</span>
+				<span>80%</span>
+				<span>90%</span>
+				<span>100%</span>
+			</div>
+		</div>
+		<ul class="topics mb-10">
+			{#each [...topics] as topic}
+				{@const topicHistory = quizHistory.filter((q) => q.topic === topic)}
+				<li>
+					<button
+						class="btn"
+						on:click={() => {
+							quiz = new QuizType(
+								parsedQuestions,
+								topic,
+								numberOfQuestions[numberOfQuestionsIndex],
+								passingGrade
+							);
+							quizActive = true;
+						}}
+					>
+						{topic}
+					</button>
+					{#if topicHistory.length > 0}
+						<ul class="scores">
+							{#each topicHistory as history}
+								<li class={history.passingGradeFactor <= history.score ? 'success' : 'failure'}>
+									{Math.round(history.score * 100)} %
+								</li>
+							{/each}
+						</ul>
+					{/if}
+				</li>
+			{/each}
+		</ul>
+		{#if quizHistory.length > 0}
+			<div class="text-center">
 				<button
-					class="btn"
+					class="btn btn-outline"
 					on:click={() => {
-						quiz = new QuizType(
-							parsedQuestions,
-							topic,
-							numberOfQuestions[numberOfQuestionsIndex],
-							0.75
-						);
-						quizActive = true;
+						quizHistory = [];
+						localStorage.setItem('quizHistory', JSON.stringify(quizHistory));
 					}}
 				>
-					{topic}
+					Clear history
 				</button>
-				{#if topicHistory.length > 0}
-					<ul class="scores">
-						{#each topicHistory as history}
-							<li class={history.passingGradeFactor <= history.score ? 'success' : 'failure'}>
-								{Math.round(history.score * 100)} %
-							</li>
-						{/each}
-					</ul>
-				{/if}
-			</li>
-		{/each}
-	</ul>
-	{#if quizHistory.length > 0}
-		<div class="clearHistory mt-10 text-center">
-			<button
-				class="btn btn-outline"
-				on:click={() => {
-					quizHistory = [];
-					localStorage.setItem('quizHistory', JSON.stringify(quizHistory));
-				}}
-			>
-				Clear history
-			</button>
-		</div>
-	{/if}
+			</div>
+		{/if}
+	</div>
 {/if}
 
 <style lang="postcss">
